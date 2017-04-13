4cf23020a6e240703093a0614d802b0242ea050c
Pedro Cattori <pcattori@gmail.com>
2017-04-13 17:41:11 -0400

    DataflowFunction absorbed by dataflow

--- dataflow.py
+++ dataflow.py
@@ -1,18 +1,28 @@
+def _resolve(arg, assignments={}):
+    if isinstance(arg, DataflowFunctionCall):
+        return arg.execute(assignments)
+    elif isinstance(arg, Variable):
+        return assignments[arg]
+    return arg
+
 class DataflowFunctionCall:
-    def __init__(self, func, *args):
+    def __init__(self, func, *args, **kwargs):
         self.func = func
         self.args = args
+        self.kwargs = kwargs
 
-    def execute(self):
-        args = [arg.execute() if isinstance(arg, type(self)) else arg for arg in self.args]
-        return self.func(*args)
+    def execute(self, assigments={}):
+        # step 1: resolve inputs
+        args = [_resolve(arg, assigments) for arg in self.args]
+        kwargs = {k: _resolve(v, assigments) for k, v in self.kwargs.items()}
 
-class DataflowFunction:
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, *args):
-        return DataflowFunctionCall(self.func, *args)
+        # step 2: resolve function call
+        return self.func(*args, **kwargs)
 
 def dataflow(func):
-    return DataflowFunction(func)
+    # when result is called, binds `func` to the inputs by storing in a DataflowFunctionCall
+    return lambda *args, **kwargs: DataflowFunctionCall(func, *args, **kwargs)
+
+class Variable:
+    def __hash__(self):
+        return id(self)
--- example.py
+++ example.py
@@ -1,24 +1,53 @@
-from dataflow import dataflow
+import dataflow
+
+def _resolve(arg, assignments={}):
+    if isinstance(arg, DataflowFunctionCall):
+        return arg.execute(assignments)
+    elif isinstance(arg, Variable):
+        return assignments[arg]
+    return arg
+
+class DataflowFunctionCall:
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def execute(self, assigments={}):
+        # step 1: resolve inputs
+        args = [_resolve(arg, assigments) for arg in self.args]
+        kwargs = {k: _resolve(v, assigments) for k, v in self.kwargs.items()}
+
+        # step 2: resolve function call
+        return self.func(*args, **kwargs)
+
+def dataflow(func):
+    # when result is called, binds `func` to the inputs by storing in a DataflowFunctionCall
+    return lambda *args, **kwargs: DataflowFunctionCall(func, *args, **kwargs)
+
+class Variable:
+    def __hash__(self):
+        return id(self)
 
 @dataflow
-def f(g, seed):
-    return g**2 % seed
+def f(x, y):
+    return x**2 % y
 
 @dataflow
-def g(a, b, h):
-    return a * b + h
+def g(a, b, c):
+    return a * b + c
 
 @dataflow
-def h(c, d):
-    return c / d
+def h(i, j):
+    return i / j
 
-seed = 5
-
-# ---
+z = Variable()
+seed = Variable()
 
 # setting up the execution / dataflow
-graph = f(g(1, 2, h(3, 4)), seed)
+graph = f(g(z, 2, h(4, z)), seed)
 # no addition or multiplication has happened yet
 
 # executing the dataflow
-print(graph.execute())
+print(graph.execute({z: 1., seed: 5}))
+print(graph.execute({z: 3., seed: 13}))
