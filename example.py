73b0a414f447d828fb209cb4b128ccc371f2ceaa
Pedro Cattori <pcattori@gmail.com>
2017-04-13 18:07:24 -0400

    example imports functionality instead of copying implementation

--- example.py
+++ example.py
@@ -1,33 +1,4 @@
-import dataflow
-
-def _resolve(arg, assignments={}):
-    if isinstance(arg, DataflowFunctionCall):
-        return arg.execute(assignments)
-    elif isinstance(arg, Variable):
-        return assignments[arg]
-    return arg
-
-class DataflowFunctionCall:
-    def __init__(self, func, *args, **kwargs):
-        self.func = func
-        self.args = args
-        self.kwargs = kwargs
-
-    def execute(self, assigments={}):
-        # step 1: resolve inputs
-        args = [_resolve(arg, assigments) for arg in self.args]
-        kwargs = {k: _resolve(v, assigments) for k, v in self.kwargs.items()}
-
-        # step 2: resolve function call
-        return self.func(*args, **kwargs)
-
-def dataflow(func):
-    # when result is called, binds `func` to the inputs by storing in a DataflowFunctionCall
-    return lambda *args, **kwargs: DataflowFunctionCall(func, *args, **kwargs)
-
-class Variable:
-    def __hash__(self):
-        return id(self)
+from dataflow import dataflow, Variable
 
 @dataflow
 def f(x, y):
